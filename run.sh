e611471cadc353c3fa716ea2c0902c97f2f32490
ddubovsky <ddubovsky@malwarebytes.org>
2016-09-22 09:58:30 +0300

    Add pid killer in order to prevent failure starting container after being stopped unexpectedly.

--- run.sh
+++ run.sh
@@ -10,4 +10,8 @@ fi
 
 source /etc/apache2/envvars
 tail -F /var/log/apache2/* &
+
+#Apache gets grumpy about PID files pre-existing
+rm -f /var/run/apache2/apache2.pid
+
 exec apache2 -D FOREGROUND
